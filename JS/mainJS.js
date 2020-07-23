f7d2ca5682208cb1bdad976ac1d096794d3e93fb
MineCopre <diogoah99@gmail.com>
2020-07-23 21:55:32 +0100

    Carousel and Form

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -11,18 +11,31 @@ $(function(){
 	$("#formType").change(function(){
 		switch($("#formType option:selected").val()){
 			case "message":
-				$("#print").hide();
-				$("#schedule").hide();
+			$("#print").hide();
+			$("#schedule").hide();
 			break;
 			case "print":
-				$("#message").hide();
-				$("#schedule").hide();
+			$("#message").hide();
+			$("#schedule").hide();
 			break;
 			case "schedule":
-				$("#message").hide();
-				$("#print").hide();
+			$("#message").hide();
+			$("#print").hide();
 			break;
 		}
 		$("#"+$("#formType option:selected").val()).show();
-		});
-});	
+	});
+	
+	$("#fileSubmission").change(function(){
+		alert(this.files[0].name);
+		if (this.files && this.files[0]) {
+			var reader = new FileReader();
+			
+			reader.onload = function (e) {
+				alert(e.files[0].name);
+			};
+			
+			reader.readAsDataURL(this.files[0]);
+		}
+	});	
+});
