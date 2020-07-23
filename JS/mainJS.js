663a205c9919a504d4478845490bbea244631ac9
MineCopre <diogoah99@gmail.com>
2020-07-23 15:55:25 +0100

    Form

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -1,9 +1,28 @@
 $(function(){
 	
-	$("#navMap").click(function(){
-		 $('html, body').animate({
-        scrollTop: $("#MAP").offset().top
-    }, 2000);
+	$(".nav-link").click(function(){
+		var a = $(this).attr('id');
+		a = a.replace("nav", "");
+		$('html, body').animate({
+			scrollTop: $("#"+a).offset().top
+		}, 2000);
 	});
 	
+	$("#formType").change(function(){
+		switch($("#formType option:selected").val()){
+			case "message":
+				$("#print").hide();
+				$("#schedule").hide();
+			break;
+			case "print":
+				$("#message").hide();
+				$("#schedule").hide();
+			break;
+			case "schedule":
+				$("#message").hide();
+				$("#print").hide();
+			break;
+		}
+		$("#"+$("#formType option:selected").val()).show();
+		});
 });	
