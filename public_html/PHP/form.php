0e5f20b0082c4488c2e34141d852d3d99b7f714d
MineCopre <diogoah99@gmail.com>
2021-03-30 17:04:47 +0100

    button8

--- public_html/PHP/form.php
+++ public_html/PHP/form.php
@@ -5,34 +5,29 @@
 	require 'C:\php\vendor\autoload.php';
 	
 	$mail = new PHPMailer(TRUE);
+	
+	$ip = $_SERVER['REMOTE_ADDR'];
 	$captchaURL = "https://www.google.com/recaptcha/api/siteverify?secret=6LfmF8AZAAAAAF284CXnmOJzsaZ8hZ-e9TQfHHIz";
+	$response = file_get_contents($url);
+	$responseKeys = json_decode($response, true);
 	
 	echo "yo";
-	if (isset($_POST['g-recaptcha-response'])) {
-		$captcha = $_POST['g-recaptcha-response'];
-	} 
-	else 
-	{
-		$captcha = false;
-		echo "nay1";
-	}
 	
-	if (!$captcha) {
-		
-		echo "nay2";
-	} 
-	else 
-	{
-		$secret   = '6LfmF8AZAAAAAF284CXnmOJzsaZ8hZ-e9TQfHHIz';
-		$response = file_get_contents($captchaURL . $secret . "&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
-		
-		$response = json_decode($response);
-		
-		if ($response->success === false) {
-			echo "nay";
-		}
-	
-	}
+	if ($responseKeys["success"] && $responseKeys["action"] == 'contactForm') {
+            if ($responseKeys["score"] >= 0.5) {
+                //send email with contact form submission data to site owner/ submit to database/ etc
+                //redirect to confirmation page or whatever you need to do
+            } elseif ($responseKeys["score"] < $g_recaptcha_allowable_score) {
+                //failed spam test. Offer the visitor the option to try again or use an alternative method of contact.
+            }
+        } elseif($responseKeys["error-codes"]) { //optional
+            //handle errors. See notes below for possible error codes
+            //(I handle errors by sending myself an email with the error code for debugging and offering the visitor the option to try again or use an alternative method of contact)
+        } else {
+            //unkown screw up. Again, offer the visitor the option to try again or use an alternative method of contact.
+        }
+
+        exit;
 	
 	if ($response->success==true && $response->score <= 0.5) {
     //Do something to denied access
