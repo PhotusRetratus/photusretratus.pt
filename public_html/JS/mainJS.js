b56820eab2dd5e6e9ba11fcb5088adc38fbcac0e
MineCopre <diogoah99@gmail.com>
2021-03-31 16:42:51 +0100

    testing captcha12

--- public_html/JS/mainJS.js
+++ public_html/JS/mainJS.js
@@ -1,9 +1,10 @@
 $(function(){	
 	
-	$('#contactForm').on('submit', function () {
-		e.preventDefault();
-		grecaptcha.ready(function() {
+	grecaptcha.ready(function() {
+		$('#contactForm').on('submit', function () {
+			e.preventDefault();
 			grecaptcha.execute('6LfmF8AZAAAAAFNT2ytbglM2hGpeJif1CmZgZZXm', {action:'submit'}).then(function(token) {
+				alert(token);
 				$("#g-recaptcha-response").value = token;
 				alert(token);
 			});
