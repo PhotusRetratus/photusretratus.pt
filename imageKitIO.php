82475213488132f9623a40c345b825da5cb00d64
diogoah99@gmail.com <diogoah99@gmail.com>
2021-08-27 16:45:44 +0100

    imagekitTest5

--- imageKitIO.php
+++ imageKitIO.php
@@ -9,21 +9,6 @@ if (is_file(__DIR__ . '/../vendor/autoload.php') && is_readable(__DIR__ . '/../v
     require_once __DIR__ . '/../autoload.php';
 }
 
-use ImageKit\ImageKit;
+echo "Too";
 
-$imageKit = new ImageKit(
-    "public_TGL83sxiUWGZfYFL0MMz9r7AXTw=",
-    "private_UTwuFdOJoKH3tkWbe6iko+7SB4I=",
-    "https://ik.imagekit.io/minecopre"
-);
-
-$imageURL = $imageKit->url(array(
-    "path" => $sample_file_path,
-    "transformation" => array(
-        array(
-            "height" => "300",
-            "width" => "400",
-        ),
-    ),
-));
 ?>
